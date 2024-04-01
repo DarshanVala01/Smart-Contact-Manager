71f3b5aab4f5767b86fba1dce04e4567735379d0
DarshanVala01 <darshanvala024@gmail.com>
2024-04-01 16:10:49 +0530

    smart contact manager update and delete
    update and delete facility are generated in project

--- src/main/java/com/spring/project/controller/UserController.java
+++ src/main/java/com/spring/project/controller/UserController.java
@@ -1,25 +1,37 @@
 package com.spring.project.controller;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.spring.project.entities.Contact;
 import com.spring.project.entities.User;
+import com.spring.project.helper.Message;
+import com.spring.project.repository.ContactRepository;
 import com.spring.project.repository.UserRepository;
 
-import jakarta.persistence.criteria.Path;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -27,6 +39,9 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 	
 	// method for adding common data to response
 	
@@ -63,7 +78,9 @@ public class UserController {
 	
 	@PostMapping("/contact-process")
 	public String processContact(@ModelAttribute Contact contact , 
-									@RequestParam("profile_Image") MultipartFile file , Principal principal) {
+									@RequestParam("profile_Image") MultipartFile file ,
+									Principal principal,
+									HttpSession session) {
 		
 		try {
 			//Getting userName
@@ -80,27 +97,90 @@ public class UserController {
 			else {
 				// upload the file to folder and update the name to contact
 				contact.setImageUrl(file.getOriginalFilename());
+				
 				File saveFile = new ClassPathResource("/static/images").getFile();
-				
-				
-				
+				Path path =  Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				System.out.println("Image is uploaded");
 			}
-			
 			contact.setUser(user);
 			user.getContacts().add(contact);
-			
 			this.userRepository.save(user);
-			
 			System.out.println("Added to database");
 			
-		} catch (Exception e) {
+			// Success message..
+			session.setAttribute("message", new Message("" , "success"));
 			
+		} catch (Exception e) {	
 			e.printStackTrace();
+			
+			
 		}
-		
-		
-		
 		return "user/add_contact";
 	}
 	
+	
+	// Show Contact Handler
+	// Per page = 5[n]
+	// Current Page = 0[page]
+	@GetMapping("/show-contacts/{page}")
+	public String showContacts(@PathVariable("page") Integer page , Model model , Principal principal) {
+		
+		model.addAttribute("title" , "Show Contact");
+		
+		String userName = principal.getName();
+		User user = userRepository.findByEmail(userName);
+		
+		Pageable pageable = PageRequest.of(page, 5);
+		
+		Page<Contact> contacts = this.contactRepository.findContactByUser(user.getId() , pageable);
+		
+		model.addAttribute("contacts" , contacts);
+		model.addAttribute("currentPage" , page);
+		model.addAttribute("totalPages" , contacts.getTotalPages());
+		
+		return "user/show_contacts";
+	}
+	
+	
+	// Showing specific contact details
+	@GetMapping("/contact/{cId}")
+	public String showContactDetail(@PathVariable("cId") int cId , Model model , Principal principal) {
+		
+		Optional<Contact> contactOptional = contactRepository.findById(cId);
+		Contact contact = contactOptional.get();
+		
+		String userName = principal.getName();
+		User user = this.userRepository.findByEmail(userName);
+		
+		if(user.getId()==contact.getUser().getId()) {
+			
+			model.addAttribute("contact" , contact);
+		}
+		
+		return "user/contact_detail";
+	}
+	
+	
+	// Delete contact handler
+	@GetMapping("/delete/{cId}")
+	public String deleteContact(@PathVariable("cId") int cId , Model model , HttpSession session) {
+		
+		this.contactRepository.deleteById(cId);
+		
+		session.setAttribute("message", new Message("Contact deleted successfully" , "deleted"));
+		
+		return "redirect:/user/show-contacts/0";
+	}
+	
+	// Update contact handler
+	@GetMapping("/update/{cId}")
+	public String updateContactForm(@PathVariable("cId") int cId , Model model) {
+		
+		Optional<Contact> contactOptional = this.contactRepository.findById(cId);
+		Contact contact = contactOptional.get();
+		
+		model.addAttribute("contact" , contact);
+		return "user/update_contact";
+	}
 }
